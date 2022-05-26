c3e7cfc26f8783a9a3834d4867f796b832642cd5
Taha Turan <tahaaturann16@gmail.com>
2022-05-26 22:51:19 +0300

    Burclar Rehberi ilk hazirliklar yapildi

---
+++ lib/burc_listesi.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BurcListesi extends StatelessWidget {
+  const BurcListesi({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:burc_rehberi/burc_listesi.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Burc Listesi',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+      home: const BurcListesi(),
+    );
+  }
+}
